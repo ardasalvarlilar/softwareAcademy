8e9760ce72d088c6999dbe92a9e8475f582d0539
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-10-04 18:17:28 +0300

    created models and migrated database

--- kadro/models.py
+++ kadro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+  title = models.CharField(max_length=50)
+
+  def __str__(self):
+    return f'{self.title}'
+
+class Kadro(models.Model):
+  full_name = models.CharField(max_length=100)
+  image_url = models.ImageField(upload_to='persomal-picture')
+  category = models.ManyToManyField(Category)
