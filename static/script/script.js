c3d39525546006900bc9f34205d882036b9cc47f
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-10-04 16:24:27 +0300

    js

--- static/script/script.js
+++ static/script/script.js
@@ -1,20 +1,20 @@
-const links = document.querySelectorAll('.linki') // .link sınıfını seçiyoruz
+const links = document.querySelectorAll('.border-bottom') // .link sınıfını seçiyoruz
 
 for (let i of links) {
     i.addEventListener('click', toggleLink)
 }
 
 function toggleLink() {
-    let willOpen = this.nextElementSibling
+    let willOpen = this.nextSibling
     console.log(willOpen)
 
     if (willOpen.classList.contains('hide')) {
         willOpen.classList.remove('hide')
         willOpen.classList.add('show')
-        this.querySelector('.fa-plus').classList.toggle('turn')
+        // this.querySelector('.fa-plus').classList.toggle('turn')
     } else {
         willOpen.classList.remove('show')
         willOpen.classList.add('hide')
-        this.querySelector('.fa-plus').classList.toggle('turn')
+        // this.querySelector('.fa-plus').classList.toggle('turn')
     }
 }
