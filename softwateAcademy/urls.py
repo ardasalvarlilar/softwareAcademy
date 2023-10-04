d27e73222ca7dfef265bcffc4e6f384b5a9335b1
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-10-04 14:44:50 +0300

    pages core partials

--- softwateAcademy/urls.py
+++ softwateAcademy/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('page.urls')),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
