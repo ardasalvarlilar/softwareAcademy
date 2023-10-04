650b52771703e791453518aa489a75053d8e5e84
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-10-04 17:49:25 +0300

    baya bir şey yaptım bi bak

--- pages/urls.py
+++ pages/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', index, name='indexPage'),
+    path('kadromuz/', kadromuz, name='kadromuzPage')
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
   return render(request, 'index.html')
+
+def kadromuz(request):
+  return render(request, 'kadromuz.html')
