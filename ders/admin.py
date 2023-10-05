008078e3bda02bbce0326036788a24af88b54980
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-10-05 19:48:34 +0300

    new models

--- ders/admin.py
+++ ders/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from .models import *
+from .models import City, Day, TimeSlot, EduProgram
 
-# Register your models here.
+class EduProgramAdmin(admin.ModelAdmin):
+    list_display = ('title', 'quota', 'price', 'city', 'get_days_display', 'get_time_slots_display')
+    list_filter = ('days', 'time_slots')
+    
+    def get_days_display(self, obj):
+        return ", ".join([day.name for day in obj.days.all()])
+    
+    def get_time_slots_display(self, obj):
+        return ", ".join([f"{slot.start_time} - {slot.end_time}" for slot in obj.time_slots.all()])
+    
+    get_days_display.short_description = 'Days'
+    get_time_slots_display.short_description = 'Time Slots'
+
+
+class DayAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'end_time')
 
 admin.site.register(City)
-admin.site.register(EduProgram)
+admin.site.register(Day, DayAdmin)
+admin.site.register(TimeSlot, TimeSlotAdmin)
+admin.site.register(EduProgram, EduProgramAdmin)
--- ders/models.py
+++ ders/models.py
@@ -6,9 +6,37 @@ class City(models.Model):
   def __str__(self):
     return f'{self.title}'
   
+class Day(models.Model):
+    name = models.CharField(max_length=50, choices=(
+        ('Pazartesi, Salı, Perşembe Cuma', 'PazartesiPazartesi, Salı, Perşembe Cuma'),
+        ('Cumartesi, Pazar', 'Cumartesi Pazar')
+
+    ))
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class TimeSlot(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+      return f"{self.start_time} - {self.end_time}"
+  
 class EduProgram(models.Model):
   title = models.CharField(max_length=100)
   quota = models.IntegerField()
-  date = models.DateField()
   price = models.DecimalField(max_digits=10,decimal_places=2)
   city = models.ForeignKey(City, on_delete=models.CASCADE)
+  days = models.ManyToManyField(Day)
+  time_slots = models.ManyToManyField(TimeSlot)
+
+  def __str__(self):
+    return f'{self.title} {self.city}'
+  
+
+
+
