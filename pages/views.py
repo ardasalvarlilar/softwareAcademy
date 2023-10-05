7973f83cf8bb516ef37c0f51963656c655d86146
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-10-05 10:33:37 +0300

    egitimler yorumlar

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from kadro.models import *
+from .models import *
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+    all_questions = Questions.objects.all()
+    egitimler = Education.objects.all()
+    
+    even_id_questions = [question for question in all_questions if question.id % 2 == 0]
+
+    odd_id_questions = [question for question in all_questions if not question.id % 2 == 0]
+
+    return render(request, 'index.html', {
+        'yorumlar': even_id_questions,
+        'baskaYorumlar': odd_id_questions,
+        'egitimler': egitimler
+    })
 
 def kadromuz(request):
   personel = Kadro.objects.all()
